1dcb744b96bd51ec9d4641d96755ac2b22b3bef9
Lucas Cavalcante <lucas.cavalcante.dev@icloud.com>
2024-06-16 19:50:28 -0300

    feat: add NextAuth with Google

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,5 +1,6 @@
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { signIn } from 'next-auth/react'
 
 import { Container, Header } from '../styles'
 import { ConnectBox, ConnectItem } from './styles'
@@ -22,7 +23,11 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Agenda</Text>
-          <Button variant="secondary" size="sm">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => signIn('google')}
+          >
             Conectar
           </Button>
         </ConnectItem>
