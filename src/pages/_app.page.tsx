1c50cfac9c5115ccce8eb12f7a1adaf5a8d4faa3
Lucas Cavalcante <lucas.cavalcante.dev@icloud.com>
2024-06-29 13:32:01 -0300

    feat: handle SEO

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -6,6 +6,7 @@ import { SessionProvider } from 'next-auth/react'
 import { globalStyles } from '../styles/global'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '../lib/react-query'
+import { DefaultSeo } from 'next-seo'
 
 globalStyles()
 
@@ -16,6 +17,15 @@ export default function App({
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://www.lucas-agenda.com.br',
+            siteName: 'Ignite Cal - CtsLucas',
+          }}
+        />
+
         <Component {...pageProps} />
       </SessionProvider>
     </QueryClientProvider>
